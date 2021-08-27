01f45090ea5bdb714aabd0cdb0b6440411bde153
anbucabcs <anbua@cabcsgroup-india.com>
2021-08-27 16:03:01 +0530

    Employee Import new Fields Added

--- app/Imports/EmployeesImport.php
+++ app/Imports/EmployeesImport.php
@@ -9,6 +9,12 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use App\mJobTitle;
+use App\mJobCategory;
+use App\mCompanyLocation;
+use App\tEmployeeReportTo;
+use App\mCountry;
 
 class EmployeesImport implements ToModel, WithStartRow
 {
@@ -38,6 +44,20 @@ class EmployeesImport implements ToModel, WithStartRow
         $country = $row[13];
         $home_telephone = $row[14];
         $mobile = $row[15];
+        $work_telephone = $row[16];
+        $alternate_email = $row[17];
+        $status = $row[18];
+        $job_title = $row[19];
+        $job_category = $row[20];
+        $date_of_join = $row[21];
+        $company_name = $row[22];
+        $company_country = $row[23];
+        $company_state = $row[24];
+        $company_city = $row[25];
+        $company_address = $row[26];
+        $report_to = $row[27]; //Emmployee_id Comma Separated
+        $user_role = $row[28];
+        $user_password = $row[29];
         //$isExists = User::where('email', $email)->first();
         $isExistsEmployee = Employee::where('employee_id', $employee_id)->orWhere('email', $email)->withTrashed()->first();
 
@@ -51,15 +71,88 @@ class EmployeesImport implements ToModel, WithStartRow
 
             // $user->assignRole('Employee');
 
+            //job_title
+            
+            //check the job_title is there
+            $jobTitle = mJobTitle::where('job_title', $job_title)->first();
+            if(!$jobTitle && $job_title) {
+                //insert job title
+                $jobTitle = new mJobTitle;
+                $jobTitle->job_title = $job_title;
+                $jobTitle->save();
+                $job_id = $jobTitle->id;
+            }
+            $job_id = ($jobTitle) ? $jobTitle->id : NULL; 
+            //job_category
+            $jobCategory = mJobCategory::where('name', $job_category)->first();
+            if(!$jobCategory && $job_category) {
+                $jobCategory = new mJobCategory;
+                $jobCategory->name = $job_category;
+                $jobCategory->save();
+            }
+            $job_category_id = ($jobCategory) ? $jobCategory->id : NULL;
+
+            $locationId = NULL;
+            $companyLocation = mCompanyLocation::where('company_name', $company_name)->first();
+            if($companyLocation) {
+                $locationId = $companyLocation->id;
+            } else {
+                //insert Company
+                $countryDet = mCountry::where('country', $company_country)->first();
+                $countryId = ($countryDet) ? $countryDet->id : NULL;
+                if($countryId && $company_name && $company_country && $company_state && $company_city) {
+                   $company = new mCompanyLocation;
+                   $company->company_name = $company_name;
+                   $company->country_id = $countryId;
+                   $company->state_province = $company_state;
+                   $company->city = $company_city;
+                   $company->address = ($company_address) ? $company_address : '';
+                   $company->zip_code = '';
+                   $company->phone_number = '';
+                   $company->fax = '';
+                   $company->notes = '';
+                   $company->save();
+                   $locationId = $company->id;
+                }
+            }
+
+            $user_id = NULL;
+            if($user_password) {
+                $isExists = User::where('email', $email)->first();
+                if($isExists) {
+                    // 'Email already Exists');        
+                } else {
+                    $user = new User;
+                    $user->name = $first_name . ' '.$last_name;
+                    $user->email = $email;
+                    $user->password =  Hash::make($user_password);
+                    $user->save();  
+
+                    $user_id = $user->id;
+
+                    if($user_role == 'Employee' || $user_role == 'Admin' || $user_role == 'Manager') {
+                        $user->assignRole($user_role); 
+                    } else {
+                        $user->assignRole('Employee');                  
+                    }
+                }
+                
+            }
+
             $employee = new Employee;
-            $employee->user_id = NULL;
+            $employee->user_id = $user_id;
             $employee->first_name = $first_name;
             $employee->middle_name = $middle_name;
             $employee->last_name = $last_name;
             $employee->email = $email;
             $employee->employee_id = $employee_id;
-            $employee->status = 'Active';
+            $employee->status = ($status == 'in active' || $status == 'In active' || $status == 'In Active') ? 'In active' : 'Active';
             $employee->gender = $gender;
+            $employee->joined_date = date('Y-m-d', strtotime($date_of_join));
+            $employee->date_of_birth = date('Y-m-d', strtotime($dob));
+            $employee->job_id = $job_id;
+            $employee->job_category_id = $job_category_id;
+            $employee->company_location_id = $locationId;
             $employee->created_by = Auth::User()->id;
             $employee->updated_by = Auth::User()->id;        
             $employee->save();
@@ -74,7 +167,26 @@ class EmployeesImport implements ToModel, WithStartRow
             $contact->country = $country;
             $contact->home_telephone = $home_telephone;
             $contact->mobile = $mobile;
+            $contact->work_telephone = $work_telephone;
+            $contact->alternate_email = $alternate_email;
             $contact->save();
+
+            //Report To
+            if($report_to) {
+                $managerIds = explode(',', $report_to);
+                if(count($managerIds)) {
+                    foreach ($managerIds as $manager) {
+                        //get employee primary id in employees table
+                        $managerEmp = Employee::where('employee_id', $manager)->first();
+                        if($managerEmp) {
+                            $report = new tEmployeeReportTo;
+                            $report->employee_id = $employee->id;
+                            $report->manager_id = $managerEmp->id;
+                            $report->save();                            
+                        }
+                    }
+                }
+            }            
 
             $this->results['success'][] = ['name' => $first_name . ' '.$middle_name.' '.$last_name, 'email' => $email];
             ++$this->rows;
