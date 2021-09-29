0806dcc3f98f8b5ab1d41bd72157d142f307ef08
anbucabcs <anbua@cabcsgroup-india.com>
2021-09-29 12:52:25 +0530

    Remember Me login - Completed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use Cookie;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,26 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) {
+
+        request()->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        
+        $remember_me = $request->has('remember') ? true : false;
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')], $remember_me))
+        {
+            if($remember_me) {
+                setcookie('cit_login_user_name', base64_encode($request->input('email')), time()+30*24*60*60, "/"); //30 day
+                setcookie('cit_login_password', base64_encode($request->input('password')), time()+30*24*60*60, "/"); //30 day
+            }            
+            // Authentication passed...
+            return redirect('/home');
+        }
+        return redirect()->back()->withErrors(['email' => 'These credentials do not match our records.']);        
     }
 }
