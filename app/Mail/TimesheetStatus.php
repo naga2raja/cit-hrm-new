cfdb1233482bc9af93cb81756e0ccec79dc3fa32
anbucabcs <anbua@cabcsgroup-india.com>
2021-11-30 15:30:32 +0530

    Err fix

--- app/Mail/TimesheetStatus.php
+++ app/Mail/TimesheetStatus.php
@@ -11,12 +11,14 @@ class TimesheetStatus extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $details;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($details)
     {
         $this->details = $details;
     }
